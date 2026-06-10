6e06aa7ccbfd800cf5b7f25cc258e62887c0061f
eleonore141999-cell <eleonore.14.1999@gmail.com>
2026-06-10 11:21:00 +0200

    first changes: linting, debugging

--- Circular barplot descriptives MED.R
+++ Circular barplot descriptives MED.R
@@ -1,4 +1,5 @@
 # Descriptive statistics barchart from R graphgallery for medication frequencies
+source("Simulated dataset for barplot example.R")
 
 library(dplyr)
 library(ggplot2)
@@ -8,7 +9,7 @@ library(tidyverse)
 med_count <- with(dta, table(MED))
 print(med_count)
 med_prop <- prop.table(med_count)
-med_pct <- med_prop*100
+med_pct <- med_prop * 100
 print(med_pct)
 
 plot_med <- dta %>%
@@ -25,15 +26,17 @@ plot_med_clean <- plot_med %>%
 # View the result
 print(plot_med)
 
-#Basic radar chart
+# Basic radar chart
 plot_final <- ggplot(plot_med, aes(x = factor(MED), y = count)) +
-  labs(title = "Frequency of MED types",
-       x = "Type of medication",
-       y = "Count") +
+  labs(
+    title = "Frequency of MED types",
+    x = "Type of medication",
+    y = "Count"
+  ) +
   theme_minimal() +
   # Make custom panel grid
   geom_hline(
-    aes(yintercept = y), 
+    aes(yintercept = y),
     data.frame(y = c(0, 500, 1000, 1500, 2000, 2500)),
     color = "lightgrey"
   ) +
@@ -49,32 +52,32 @@ plot_final <- ggplot(plot_med, aes(x = factor(MED), y = count)) +
     position = "dodge2",
     show.legend = TRUE,
     alpha = .9
-  ) + 
-    # ADDED: Percentages next to each bar
-    geom_text(
-      aes(
-        x = reorder(str_wrap(as.character(MED), 9), count),
-        y = count-200, 
-        label = paste0(round(med_pct, 1), "%")
-      ),
-      size = 3,
-      color = "black"
-    ) +
-    ylim(0, 2500) +
-  
-  # Make it circular!
-    coord_polar()
+  ) +
+  # ADDED: Percentages next to each bar
+  geom_text(
+    aes(
+      x = reorder(str_wrap(as.character(MED), 9), count),
+      y = count - 200,
+      label = paste0(round(med_pct, 1), "%")
+    ),
+    size = 3,
+    color = "black"
+  ) +
+  ylim(0, 2500) +
 
-plot_final2 <- plot_final + 
+  # Make it circular!
+  coord_polar()
+
+plot_final2 <- plot_final +
   # Annotate custom scale inside plot if wanted
-  #annotate(
-   #x = 9, 
-    #y = 250, 
-    #label = "250", 
-    #geom = "text", 
-    #color = "gray12", 
-    #family = "sans"
-  #) +
+  # annotate(
+  # x = 9,
+  # y = 250,
+  # label = "250",
+  # geom = "text",
+  # color = "gray12",
+  # family = "sans"
+  # ) +
 
   # Scale y axis so bars don't start in the center
   scale_y_continuous(
@@ -82,21 +85,22 @@ plot_final2 <- plot_final +
     expand = c(0, 0),
     breaks = c(0, 500, 1000, 1500, 2000, 2500),
     position = "left"
-  ) + 
-  # New fill and legend title for number of tracks per region. 
+  ) +
+  # New fill and legend title for number of tracks per region.
   # To use category counts instead of 0,1000 this is the code: min(plot_med$count), max(plot_med$count)
   scale_fill_gradientn(
     "Amount of Patients",
-    colours = c( "#6C5B7B","#C06C84","#F67280","#F8B195"), 
-    limits = c(0,2500) 
-  )  + 
+    colours = c("#6C5B7B", "#C06C84", "#F67280", "#F8B195"),
+    limits = c(0, 2500)
+  ) +
   # Make the guide for the fill discrete
   guides(
     fill = guide_colorsteps(
       barwidth = 2, barheight = 5, title.position = "top", title.hjust = .5
-  ))
+    )
+  )
 
-plot_final3 <-plot_final2 +
+plot_final3 <- plot_final2 +
 
   # Add labels
   labs(
@@ -104,18 +108,18 @@ plot_final3 <-plot_final2 +
     subtitle = paste(
       "\nCount and percentages of patients using that medication class"
     ),
-    caption = "By Eleonore Logie \n Dataset simulated by Gemini AI") +
+    caption = "By Eleonore Logie \n Dataset simulated by Gemini AI"
+  ) +
   # Customize general theme
   theme(
-    
     # Set default color and font family for the text
     text = element_text(color = "gray12", family = "sans"),
-    
+
     # Customize the text in the title, subtitle, and caption
     plot.title = element_text(face = "bold", size = 25, hjust = 0.05),
     plot.subtitle = element_text(size = 12, hjust = 0.05),
     plot.caption = element_text(size = 10, hjust = .5),
-    
+
     # Make the background white and remove extra grid lines
     panel.background = element_rect(fill = "white", color = "white"),
     panel.grid = element_blank(),
@@ -123,6 +127,3 @@ plot_final3 <-plot_final2 +
   )
 # Use `ggsave("plot.png", plt,width=9, height=12.6)` to save it as in the output
 plot_final3
-
-
-
